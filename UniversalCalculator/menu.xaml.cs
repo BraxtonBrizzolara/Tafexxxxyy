8440bfb6f5cc3c2a710f41ae49c8f54112c8e318
001224207 <jordan.WALLACE3@student.tafesa.edu.au>
2026-08-31 13:20:09 +0930

    added missing files from last commit

---
+++ UniversalCalculator/menu.xaml.cs
@@ -0,0 +1,43 @@
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class BlankPage1 : Page
+	{
+		public BlankPage1()
+		{
+			InitializeComponent();
+		}
+
+		private void maths_Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Morgage_button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void currency_button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void exit_button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
